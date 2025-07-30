9aeff1518e2e93a5576090c5337bc71e46ad6ca7
David Gomes de Carvalho <davidgms31@gmail.com>
2025-07-30 14:39:37 -0300

    challenge resolved

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log("Your app is listening on port " + port);
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	const file = req.file;
+	const fileName = file.originalname;
+	const fileType = file.mimetype;
+	const fileSize = file.size;
+
+	const uploadRes = {
+		name: fileName,
+		type: fileType,
+		size: fileSize,
+	};
+	res.json(uploadRes);
 });
